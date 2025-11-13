ca9a1c49bbabc2ba767b9781c94bb04b3d1d0a9e
Ciubuc Ioana Teodora <162925864+teodoraciubuc@users.noreply.github.com>
2025-11-14 00:01:31 +0200

    Am adaugat tema 6

---
+++ seminar6/3/index.js
@@ -0,0 +1,10 @@
+let evenCollection = document.querySelectorAll("tbody tr:nth-child(even)");
+let oddCollection = document.querySelectorAll("tbody tr:nth-child(odd)");
+
+if (evenCollection && evenCollection.length > 0) {
+  for (let item of evenCollection) item.bgColor = "white";
+}
+
+if (oddCollection && oddCollection.length > 0) {
+  for (let item of oddCollection) item.bgColor = "plum";
+}
